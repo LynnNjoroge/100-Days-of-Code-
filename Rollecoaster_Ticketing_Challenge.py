58e28623b81d688747b2cf717da49ba12647f7d3
Lynn Njoroge <73690705+LynnNjoroge@users.noreply.github.com>
2022-01-22 15:36:41 +0300

    Update Rollecoaster_Ticketing_Challenge.py

--- Rollecoaster_Ticketing_Challenge.py
+++ Rollecoaster_Ticketing_Challenge.py
@@ -12,6 +12,10 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+    elif age >= 45 and age <+ 55:
+        # this is a person going through a mid-life crisis
+        print("Everything is going to be okay.\n Have a free ride on us")
+
     else:
         bill = 12
         print("Adult tickets are $12.")
