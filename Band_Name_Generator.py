af53c4465845802eabb2530fbf593e4775ab2e39
Lynn Njoroge <73690705+LynnNjoroge@users.noreply.github.com>
2022-01-29 18:18:29 +0300

    Create Band_Name_Generator.py

---
+++ Band_Name_Generator.py
@@ -0,0 +1,11 @@
+# Create a greeting for your program.
+print("Hello and Welcome to the Band Name Generator")
+# Ask the user for his or her hometown
+home_town = input("What is the name of your hometown?\n")
+# Ask the user fpr the name of their childhood pet
+childhood_pet = input("What is the name of your childhood pet?\n")
+# Combine the name of the flower and the pet to show them their band name
+print("The name of your Band could be: " + home_town + " " + childhood_pet)
+# Make sure the input cursor shows on a new line
+
+
