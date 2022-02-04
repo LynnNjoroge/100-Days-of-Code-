c066957bfa9badba24049bfd4201e2a7de3d92d4
Lynn Njoroge <73690705+LynnNjoroge@users.noreply.github.com>
2022-02-04 06:46:10 +0300

    Update FizzBuzz_Challenge.py

--- FizzBuzz_Challenge.py
+++ FizzBuzz_Challenge.py
@@ -1,3 +1,5 @@
+#FizzBuzz Challenge
+
 '''Write a short program that prints each number from 1 to 100 on a new line. 
 For each multiple of 3, print "Fizz" instead of the number. 
 For each multiple of 5, print "Buzz" instead of the number. 
