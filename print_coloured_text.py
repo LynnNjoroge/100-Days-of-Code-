fa5504d2a50882af71734340f04ecf7b667ea244
Lynn Njoroge <73690705+LynnNjoroge@users.noreply.github.com>
2022-02-21 21:01:48 +0300

    Create print_coloured_text.py

---
+++ print_coloured_text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Lynn Njoroge "+ Fore.YELLOW+ Back.BLUE+"I love Python")
+print(Back.CYAN+"Hi My name is Lynn Njoroge")
+print(Fore.RED + Back.GREEN+ "Hi My name is Lynn Njoroge")
